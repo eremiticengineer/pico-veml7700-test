2950a4dd61f8f924cc79026fb4d7e4ae3111c83c
eremiticengineer <dev@eremiticengineer.com>
2026-08-28 10:16:56 +0100

    Remove i2c pins as they're not used

--- src/VEML7700/VEML7700.h
+++ src/VEML7700/VEML7700.h
@@ -5,7 +5,7 @@
 
 class VEML7700 {
 public:
-    VEML7700(i2c_inst_t *i2c, uint8_t address = 0x10, uint sda_pin = 2, uint scl_pin = 3);
+    VEML7700(i2c_inst_t *i2c, uint8_t address = 0x10);
 
     bool begin();
     bool readLux(float &lux);
@@ -14,7 +14,6 @@ public:
 private:
     i2c_inst_t *i2c;
     uint8_t i2c_address;
-    uint sda_pin, scl_pin;
 
     bool writeRegister(uint8_t reg, uint16_t value);
     bool readRegister(uint8_t reg, uint16_t &value);
