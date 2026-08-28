e77dba48f52746e782eaa43bd0ae6cff69c8308f
eremiticengineer <dev@eremiticengineer.com>
2026-08-28 10:17:20 +0100

    Remove i2c pins as they're not used

--- src/VEML7700/VEML7700.cpp
+++ src/VEML7700/VEML7700.cpp
@@ -9,7 +9,7 @@
 #define VEML7700_ALS_DATA    0x04
 
 VEML7700::VEML7700(i2c_inst_t *i2c_inst, uint8_t address, uint sda, uint scl)
-    : i2c(i2c_inst), i2c_address(address), sda_pin(sda), scl_pin(scl) {}
+    : i2c(i2c_inst), i2c_address(address) {}
 
 bool VEML7700::begin() {
     uint16_t config = 0x0000;
